e9d9e9975a07d63481e3eb393118b6bdc637c893
TylerLee0822 <344218053@qq.com>
2024-01-25 00:11:20 +0800

    0124

--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -1,16 +1,31 @@
 <template>
     <a-layout style="min-height: 100vh">
         <a-layout-sider :style="siderStyle" style="background: #fff">Sider</a-layout-sider>
-        <a-layout-content :style="contentStyle">Content</a-layout-content>
+        <a-layout-content :style="contentStyle">
+            <canvas id="canvas"></canvas>
+        </a-layout-content>
         <a-layout-sider :style="siderStyle" style="background: #fff">Sider</a-layout-sider>
     </a-layout>
 </template>
   
 <script>
+// import { fabric } from 'fabric';
+// import Editor from '.core';
+// const canvas = {};
+
 export default {
-    name: 'Index'
+    name: 'Index',
 }
+
+
+
+
 </script>
   
-<style ></style>
+<style>
+#canvas {
+    width: 100%;
+    height: 100%;
+}
+</style>
   
