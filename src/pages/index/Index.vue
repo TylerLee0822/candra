8973385af9a63f40a33cc9a06667a250b1de0319
TylerLee0822 <344218053@qq.com>
2024-01-24 23:20:43 +0800

    init

---
+++ src/pages/index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <a-layout style="min-height: 100vh">
+        <a-layout-sider :style="siderStyle" style="background: #fff">Sider</a-layout-sider>
+        <a-layout-content :style="contentStyle">Content</a-layout-content>
+        <a-layout-sider :style="siderStyle" style="background: #fff">Sider</a-layout-sider>
+    </a-layout>
+</template>
+  
+<script>
+export default {
+    name: 'Index'
+}
+</script>
+  
+<style ></style>
+  
